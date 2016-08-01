cf8bfc5a7e6297e417c06e132e4f9adbd94747b0
Joe <joejbajoe@gmail.com>
2016-08-01 15:43:52 +0100

    Wrote code in MainMenu to render buttons.

--- states/main_menu.js
+++ states/main_menu.js
@@ -1,5 +1,21 @@
 var MainMenu = class {
-	render() {
-		alert('Rendering MainMenu');
+	var buttons;
+
+	preload() {
+		buttons = [
+		];
+
+		for (var i = buttons.length - 1; i >= 0; i--) {
+			button = buttons[i];
+			game.load.spritesheet(button, 'assets/buttons/' + button + '.png', 193, 71);
+		}
+		
+	}
+
+	create() {
+		for (var i = buttons.length - 1; i >= 0; i--) {
+			button = buttons[i];
+			game.add.button(game.world.centerX, (i * 100) + 300, button, null, this, 2, 1, 0);
+		}
 	}
 };
