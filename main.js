e5e7e644e7d48393dbe039694393300520574d30
Joe <joejbajoe@gmail.com>
2016-08-01 15:55:04 +0100

    Removed "http:" from framework URL

---
+++ main.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+	var states;
+
+	//get document width and height
+	var width  = window.innerWidth;
+	var height = window.innerHeight;
+
+	var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create });
+
+	function preload () {
+		states = [
+			new MainMenu()
+		];
+
+		for (var i = states.length - 1; i >= 0; i--) {
+			var state = states[i];
+			state.preload();
+		}
+	}
+
+	function create () {
+		state = states[0];
+		state.create();
+	}
+
+};
