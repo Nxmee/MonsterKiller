f56cc78fe5b5c2991005ffbe94fca5e355ccf08f
Ajnin123 <ajnin@talktalk.net>
2016-08-02 12:37:36 +0100

    Developed row/column system for map
    20 x 20 Map of basic dirt
    Todo:
    - Random dirt
    - Add tree border

--- states/map_generator.js
+++ states/map_generator.js
@@ -1,6 +1,18 @@
-//dirt1 = 
-//dirt2 = 
-//dirt3 = 
-//tree  = 
-//tree2 =
-//tree3 = 
+//dirt1 = 1
+//dirt2 = 2
+//dirt3 = 3
+//tree  = 4
+//tree2 = 5
+//tree3 = 6
+
+map = []
+
+for (i=0, i<20, i++) {
+	row = []
+	for (i=0, i<20, i++) {
+		row.append(1)
+	}
+	map.append(row)
+}
+
+console.log(map)
