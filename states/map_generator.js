08be9a042387730e31574c8c29f4ae11b9edc1c5
Ajnin123 <ajnin@talktalk.net>
2016-08-02 12:45:01 +0100

    Should compile right

--- states/map_generator.js
+++ states/map_generator.js
@@ -5,14 +5,24 @@
 //tree2 = 5
 //tree3 = 6
 
+//map array
 map = []
 
-for (i=0, i<20, i++) {
+//up-down
+map_length = 20
+
+//left-right
+map_width = 20
+
+//generates map
+for (a=0; a<20; a++) {
 	row = []
-	for (i=0, i<20, i++) {
-		row.append(1)
+	for (b=0; b<20; b++) {
+		row.push(1)
 	}
-	map.append(row)
+	map.push(row)
 }
+
+//adds border
 
 console.log(map)
