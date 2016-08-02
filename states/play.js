fce6b74c9c4c4e56e6c37bfffed2e7a9bec8da5f
Ajnin123 <ajnin@talktalk.net>
2016-08-02 16:29:44 +0100

    debug for map

--- states/play.js
+++ states/play.js
@@ -43,7 +43,8 @@ var Play = {
         map_data = [].concat.apply([], generate())
         console.log(map_data);
         template['layers']['data'] = map_data
-        game.load.tilemap('tilemap', null, template, Phaser.Tilemap.TILED_JSON);
+        game.load.tilemap('tilemap', 'assets/tiles/map.json'
+, template, Phaser.Tilemap.TILED_JSON);
         game.load.image('tiles', 'assets/tiles/tilesheet.png');
         /*this.game.loadspritesheet(
             'player',
