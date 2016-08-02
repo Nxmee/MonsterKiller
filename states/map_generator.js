e23e1c21fcb8b652f6d7cd13e63da80cada53aa3
Ajnin123 <ajnin@talktalk.net>
2016-08-02 13:09:41 +0100

    Added trees in the map
    + Random Generator Functions

--- states/map_generator.js
+++ states/map_generator.js
@@ -9,29 +9,51 @@
 map = []
 
 //up-down
-map_length = 20
+map_length = 25
 
 //left-right
-map_width = 25
+map_width = 30
+
+function generateN(lower_bound, upper_bound) {
+	return lower_bound + Math.floor(Math.random() * ((upper_bound-lower_bound)+1))
+}
+
+function generateDirt() {
+	return generateN(1,3)
+}
+
+function generateTree() {
+	return generateN(4,6)
+}
 
 //generates map
 for (a=0; a<map_length; a++) {
 	row = []
 	for (b=0; b<map_width; b++) {
-		row.push(1 + Math.floor(Math.random() * 3))
+		row.push(generateDirt())
 	}
 	map.push(row)
 }
 
-//adds border
+//adds tree border
 for (i=0; i<map_width; i++) {
-	map[0][i] = 4 + Math.floor(Math.random() * 3)
-	map[map_length-1][i] = 4 + Math.floor(Math.random() * 3)
+	map[0][i] = generateTree()
+	map[map_length-1][i] = generateTree()
 }
 
 for (i=0; i<map_length; i++) {
-	map[i][0] = 4 + Math.floor(Math.random() * 3)
-	map[i][map_width-1] = 4 + Math.floor(Math.random() * 3)
+	map[i][0] = generateTree()
+	map[i][map_width-1] = generateTree()
 }
 
+//spawns trees in the map
+n_trees = generateN(2,5)
+for (i=0; i<n_trees; i++) {
+	x = generateN(1, (map_width-2))
+	y = generateN(1, (map_length-2))
+	console.log(x, y)
+	map[y][x] = generateTree()
+}
+
+//debug
 console.log(map)
