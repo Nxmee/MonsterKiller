40603abba38b9bfeebb5642828a607fa529ba322
Ajnin123 <ajnin@talktalk.net>
2016-08-02 16:02:36 +0100

    generate map

--- states/play.js
+++ states/play.js
@@ -1,24 +1,23 @@
 var Play = {
-//http://phaser.io/examples/v2/loader/load-tilemap-json
-	game: null,
+    //http://phaser.io/examples/v2/loader/load-tilemap-json
+    game: null,
 
-	preload: function (game) {
-		this.game = game;
-		game.load.tilemap('tilemap', 'assets/tiles/map,json', null, Phaser.Tilemap.TILED_JSON);
-		game.load.image('tiles', 'assets/tiles/tilesheet.png');
-		this.game.loadspritesheet(
-			'player', 
-			'assets/bob/bob_all', 
-			16, 
-			16
-		);
-	},
-	var map;
-	create: function () {
-		map = game.add.tilemap('tilemap');
-		map.addTilesetImage('tilesheet1', 'tiles');
-	},
+    preload: function(game) {
+        this.game = game;
+        game.load.tilemap('tilemap', 'assets/tiles/map,json', [].concat.apply([], generate()), Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles', 'assets/tiles/tilesheet.png');
+        this.game.loadspritesheet(
+            'player',
+            'assets/bob/bob_all',
+            16,
+            16
+        );
+    },
+    var map;
+    create: function() {
+        map = game.add.tilemap('tilemap');
+        map.addTilesetImage('tilesheet1', 'tiles');
+    },
 
-	update: function () {
-	}
+    update: function() {}
 };
