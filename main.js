9e8ee03606208c91cfa4c5bfab5fe91eee5d26d6
Nxmee <github@nxmee.com>
2016-08-03 16:02:16 +0100

    Removed Preload Completely]

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	var width  = 800;
 	var height = 600;
 	console.log("Before Play");
-	var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create });
+	var game = new Phaser.Game(width, height, Phaser.AUTO, '', { create: create });
 	game.state.add('main_menu',    MainMenu);
 	game.state.add('options_menu', OptionsMenu);
 	game.state.add('play',         Play);
