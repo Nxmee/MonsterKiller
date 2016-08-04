70ba2115dcebbbed0eb243e7de0100129db5121a
Ajnin123 <ajnin@talktalk.net>
2016-08-04 14:16:32 +0100

    Added the site
    use python server.py to run
    cd in the static/style folder then
    stylus -u jeet main.styl -w to compile

---
+++ MonsterKillerSite/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		email = request.form['email']
+		exists = False
+		with open("emails.txt", "r+") as emails:
+			for line in emails:
+				print email
+				print line
+				if email.strip().lower() == line.strip().lower():
+					exists = True
+					print "common"
+			if not exists:
+				emails.write(email+'\n')
+	return render_template('index.html')
+
+app.run(debug=True)
