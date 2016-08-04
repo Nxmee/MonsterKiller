03f05d90f953ef41cb0181a210b12e5b8b249eb2
Ajnin123 <ajnin@talktalk.net>
2016-08-04 11:51:16 +0100

    utilities

--- classes/map_generator.js
+++ classes/map_generator.js
@@ -47,10 +47,6 @@ var sides = {
     }
 }
 
-function generateN(lower_bound, upper_bound) {
-    return lower_bound + Math.floor(Math.random() * ((upper_bound - lower_bound) + 1))
-}
-
 function generateDirt() {
     return generateN(1, 3)
 }
