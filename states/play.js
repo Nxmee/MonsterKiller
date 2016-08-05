ac957bc09fdc483d6b508ca713179082fa04e7a3
Ajnin123 <ajnin@talktalk.net>
2016-08-05 13:42:31 +0100

    redid data structure

--- states/play.js
+++ states/play.js
@@ -39,11 +39,12 @@ var moveY = 0;
 var diffX = 0;
 var diffY = 0;
 var mouseclick = false;
-var monsters = [];
+/*var monsters = [];
 var monstersprites = null;
 var monstertypes = [];
 var merchant = [];
-var raw_data;
+var raw_data;*/
+
 var Play = {
     //http://phaser.io/examples/v2/loader/load-tilemap-json
     game: null,
@@ -52,33 +53,85 @@ var Play = {
         this.game = game;
         this.game.music.load('assets/music/Gameplay.wav');
         //uncomment below for dynamic
-        raw_data = generate();
-        map_data = [].concat.apply([], raw_data);
+        this.maps = [{
+            "monsters": [
+                /*{
+                    "x":0,
+                    "y":0,
+                    "type":"slime",
+                    "fought": false
+                } */
+            ],
+            "data": [],
+            "playerp": [],
+            "merchantp": []
+        }];
+        this.cmap = 0;
+        this.maps[this.cmap]['data'] = generate();
+        map_data = [].concat.apply([], this.maps[this.cmap]['data']);
+
         template['layers'][0]['data'] = map_data
         map_data = [].concat.apply([], generate());
-        monstersprites = this.game.add.group();
+        monsters = this.maps[this.cmap]['monsters']
 
         for (i = 0; i < generateN(2, 5); i++) {
+            monsters.push({ "x": 0, "y": 0, "type": 0, "fought": false });
+        }
+
+        for (i = 0; i < this.maps[this.cmap]['monsters'].length; i++) {
             x,
-            y = spawnmonster(raw_data);
+            y = spawnmonster(this.maps[this.cmap]['data']);
             ok = true;
-            for (a = 0; a < monsters.length; a++) {
-                if (monsters[a][0] == x) {
-                    if (monsters[a][1] == y) {
-                        ok = false;
-                    }
+            if (monsters[i]['x'] == x) {
+                if (monsters[i]['y'] == y) {
+                    ok = false;
                 }
             }
+
             if (ok) {
-                monsters.push([x, y])
+                monstertyperand = Math.floor(Math.random() * 4.0);
+                switch (monstertyperand) {
+                    case 0:
+                        monsters[i] = {
+                            "x": x,
+                            "y": y,
+                            "type": "slime",
+                            "fought": false
+                        }
+                        break;
+                    case 1:
+                        monsters[i] = {
+                            "x": x,
+                            "y": y,
+                            "type": "blobby",
+                            "fought": false
+                        }
+                        break;
+                    case 2:
+                        monsters[i] = {
+                            "x": x,
+                            "y": y,
+                            "type": "bat",
+                            "fought": false
+                        }
+                        break;
+                    case 3:
+                        monsters[i] = {
+                            "x": x,
+                            "y": y,
+                            "type": "spider",
+                            "fought": false
+                        }
+                        break;
+
+                }
             }
         }
 
-        x, y = spawnmonster(raw_data)
-        merchant = [x, y]
+        x,
+        y = spawnmonster(this.maps[this.cmap]['data'])
+        this.maps[this.cmap]['merchantp'] = [x, y]
 
-        // template['layers']['data'] = map_data
-        // game.load.tilemap('tilemap', null, template, Phaser.Tilemap.TILED_JSON);
         this.game.load.tilemap('map1', null, template, Phaser.Tilemap.TILED_JSON);
         n = generateN(1, 2000).toString();
         this.game.load.image('tiles', 'assets/images/tiles/tilesheet.png?' + n);
@@ -91,7 +144,6 @@ var Play = {
     },
 
     create: function() {
-        //this.game = game;
         this.game.music.play();
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         var map = null;
@@ -104,41 +156,21 @@ var Play = {
         bob = this.game.add.sprite(320, 320, 'player');
         bob.anchor.x = 0.5;
         bob.anchor.y = 0.5;
-        var monstertyperand = 0;
+        //var monstertyperand = 0;
+        monsters = this.maps[this.cmap]['monsters']
         for (i = 0; i < monsters.length; i++) {
-            var rand = Math.random() * 4.0
-                //console.log(rand);
-            monstertyperand = Math.floor(rand);
-            //console.log(monstertyperand);
-            switch (monstertyperand) {
-                case 0:
-                    // console.log("0")
-                    //monsterssprites.create(monsters[i][0], monsters[i][1], 'slime');
-                    this.game.add.sprite(monsters[i][0], monsters[i][1], 'slime');
-                    monstertypes.push('slime');
-                    break;
-                case 1:
-                    // console.log("1")
-                    //monsterssprites.create(monsters[i][0], monsters[i][1], 'blobby');
-                    this.game.add.sprite(monsters[i][0], monsters[i][1], 'blobby');
-                    monstertypes.push('blobby');
-                    break;
-                case 2:
-                    // console.log("2")
-                    //monsterssprites.create(monsters[i][0], monsters[i][1], 'bat');
-                    this.game.add.sprite(monsters[i][0], monsters[i][1], 'bat');
-                    monstertypes.push('bat');
-                    break;
-                case 3:
-                    // console.log("3")
-                    //monsterssprites.create(monsters[i][0], monsters[i][1], 'spider');
-                    this.game.add.sprite(monsters[i][0], monsters[i][1], 'spider');
-                    monstertypes.push('spider');
-                    break;
-                    // monstersprites.create(monsters[i][0], monsters[i][1], 'spider');
-            };
+            this.game.add.sprite(
+                monsters[i]['x'],
+                monsters[i]['y'],
+                monsters[i]['type']
+            );
         }
-        this.game.add.sprite(merchant[0], merchant[1], 'merchant')
+
+        this.game.add.sprite(
+            this.maps[this.cmap]['merchantp'][0],
+            this.maps[this.cmap]['merchantp'][1],
+            'merchant'
+        );
     },
 
     update: function() {
@@ -159,17 +191,18 @@ var Play = {
 
         var collision = false;
         var mcollision = false;
+        monsters = this.maps[this.cmap]['monsters'];
 
         for (a = 0; a < monsters.length; a++) {
             for (i = 0; i < player.length; i++) {
-                if ((monsters[a][0] / 32) == player[i][0] && (monsters[a][1] / 32) == player[i][1]) {
-                    mcollision = monstertypes[a];
+                if ((monsters[a]['x'] / 32) == player[i][0] && (monsters[a]['y'] / 32) == player[i][1]) {
+                    mcollision = monsters[a]['type'];
                 }
             }
         }
 
         for (b = 0; b < player.length; b++) {
-            if (raw_data[player[b][1]][player[b][0]] > 3) {
+            if (this.maps[this.cmap]['data'][player[b][1]][player[b][0]] > 3) {
                 collision = b
             }
         }
